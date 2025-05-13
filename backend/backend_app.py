b4202aca1d5651c7ba274a4fec07f96250f0246a
MoBa75 <barison@gmx.de>
2025-05-13 22:03:56 +0200

    Create add function, delete function and new_id function

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_cors import CORS
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -10,10 +11,46 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def create_new_id():
+    """
+    Creates a new unique ID for a blog entry,
+    fills in missing ID numbers.
+    :return: A unique ID numer as integer.
+    """
+    id_lst = [int(post["id"]) for post in POSTS if "id" in post
+              and str(post.get("id", "")).isdigit()]
+    new_id = next(num for num in range(1, len(POSTS)+2) if num not in id_lst)
+    return new_id
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    if request.method == 'POST':
+        new_id = create_new_id()
+        data = request.get_json()
+        title = data.get("title")
+        content = data.get("content")
+        if not title or not content:
+            return jsonify({"Error": "Title and content needed!"})
+        new_post = {"id": new_id,
+                    "title": title,
+                    "content": content
+                    }
+        POSTS.append(new_post)
+        return jsonify(new_post)
     return jsonify(POSTS)
 
 
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    for index, post in enumerate(POSTS):
+        if post["id"] == post_id:
+            POSTS.pop(index)
+            return jsonify({"message": f"Post with id {post_id} has been deleted successfully."})
+    return jsonify({"Error": f"Post with id {post_id} not found."})
+
+
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5004, debug=True)
